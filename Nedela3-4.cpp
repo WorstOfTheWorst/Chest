943d48e470478407ad14efcb27a545fa6aefb9a7
WorstOfTheWorst <61825126+WorstOfTheWorst@users.noreply.github.com>
2020-04-13 23:58:10 +0300

    Add files via upload

--- Nedela3-4.cpp
+++ Nedela3-4.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include <math.h> 
+#include <fstream>
 
+using namespace std;
 
 int main()
 {
+	std::fstream file; // создаем объект класса ifstream
+	file.open("Nedela3-4_Storage.txt",std::ios::out); // открываем файл
+	//file<<""<<std::endl;
 
 	int Zadanie1,y;
 	std::cout << "Zadaniye 1" << std::endl << std::endl;
@@ -12,21 +17,25 @@ int main()
 
 	std::cin >>  Zadanie1;
     for(int i=1; i < Zadanie1+1; i++){
-       for(int j=0; j < i; j++)
-           std::cout << 0;
+       for(int j=0; j < i; j++) 
+		   std::cout << 0;
        std::cout << std::endl;
 	}
-   std::cout << std::endl;
+   std::cout << std::endl; 
 
 //2с Задание Протабулировать функцию (шаг и диапазон задаёт пользователь):
+    std::cout << "Zadaniye 2c" << std::endl << std::endl;
+    file<<"Zadaniye 2c"<<std::endl;
     float dx, xs, xf;
     const float eps = 0.000001;  // всё, что меньше этого числа считаем нулём
 
     std::cout << "Enter [xs, xf]: ";
     std::cin >> xs >> xf;
+	file<< xs << xf <<std::endl;
 
     std::cout << "Enter dx: ";
     std::cin >> dx;
+	file<< dx <<std::endl;
 
     // заголовок таблицы
     std::cout << "\tx\t\ty" << std::endl;
@@ -36,11 +45,18 @@ int main()
     float x = xs;  // начинаем считать с левого края интервала
     while (x < xf){
        if(fabs(x - 2) < eps)
+	   {
            std::cout << "\t"
                      << x
                      << "\t\t Divide on zero"
                      << std::endl;
-       else {
+			    file << "\t"
+			         << x
+			         << "\t\t Divide on zero"
+			         << std::endl;
+	   }
+       else 
+	   {
 		   f = -x*(3+2*x)*(3+2*x)+3*x+sin(2*x);
 
            std::cout << "\t"
@@ -48,11 +64,16 @@ int main()
                      << "\t\t"
                      << f
                      << std::endl;
+				file << "\t"
+			         << x
+			         << "\t\t"
+			         << std::endl;
        }
        x += dx;
     }
 //2k Задание Протабулировать функцию (шаг и диапазон задаёт пользователь):
-
+	std::cout << "Zadaniye 2k" << std::endl << std::endl;
+    file<<"Zadaniye 2k"<<std::endl;
     std::cout << "Enter [xs, xf]: ";
     std::cin >> xs >> xf;
 
@@ -66,11 +87,18 @@ int main()
     x = xs;  // начинаем считать с левого края интервала
     while (x < xf){
        if(fabs(x - 2) < eps)
+	   {
            std::cout << "\t"
                      << x
                      << "\t\t Divide on zero"
                      << std::endl;
-       else {
+			    file << "\t"
+			         << x
+			         << "\t\t Divide on zero"
+			         << std::endl;
+	   }
+       else 
+	   {
 		   f = -x*(3+2*x)*(3+2*x)+3*x+sin(2*x);
 
            std::cout << "\t"
@@ -78,24 +106,38 @@ int main()
                      << "\t\t"
                      << f
                      << std::endl;
+		        file << "\t"
+			         << x
+			         << "\t\t"
+			         << std::endl;
        }
        x += dx;
     }
 //4f Задание Дана функция y=f(x). Найти значение функции по x:
+	std::cout << "Zadaniye 4f" << std::endl << std::endl;
+    file<<"Zadaniye 4f"<<std::endl;
+
 	std::cout << "X";
 	std::cin >> x;
-	if (x>5) { std::cout<<"y = " << 2*(x*x)+10 << std::endl; }
-	else {std::cout << "X !>5,Function does not exist"; }
-	std::cout<<"y = " << 0 << std::endl;
-	std::cout << "X";
+	if (x>5) { std::cout<<"y = " << 2*(x*x)+10 << std::endl; file<<"y = "<< 2*(x*x)+10 <<std::endl;}
+	else {std::cout << "X !>5,Function does not exist"; file<< "X !>5,Function does not exist" <<std::endl;}
+	std::cout<<"y = " << 0 << std::endl; file<<"y = " << 0 << std::endl;
+	std::cout << "X";file << "X";
 	std::cin>> x;
-	if (x<5) { std::cout<<"y = " << 2*abs(x+6)-(x*x) << std::endl; }
-	else {std::cout << "X !<5,Function does not exist"; }
+	if (x<5) { std::cout<<"y = " << 2*abs(x+6)-(x*x) << std::endl; file<<"y = "<< 2*abs(x+6)-(x*x) <<std::endl;}
+	else {std::cout << "X !<5,Function does not exist"; file<< "X !<5,Function does not exist" <<std::endl; }
 
 //Задание. Имеется товар в ящиках по 16,17,21 кг. 
 //Как получить 185 кг этого товара, не вскрывая ящики.
+	std::cout << "Zadaniye pro towar" << std::endl << std::endl;
+    file<<"Zadaniye pro towar"<<std::endl;
+
 	std::cout << "16*4+21*6" << std::endl;
+	file<<"16*4+21*6"<<std::endl;
 //3f Пользователь задаёт число Ч и Х. Посчитать y:
+	std::cout << "Zadaniye 3f" << std::endl << std::endl;
+    file<<"Zadaniye 3f"<<std::endl;
+
 	int chislo, kolichestvo;
 	y = 0;
 	std::cout <<"chislo, kolichestvo ";
@@ -105,7 +147,11 @@ int main()
 	y += -kolichestvo*chislo;
 	}
 	std::cout<<"y = " << y << std::endl;
+	file<<"y = " << y << std::endl;
 //3j
+	std::cout << "Zadaniye 3j" << std::endl << std::endl;
+	file << "Zadaniye 3j" << std::endl << std::endl;
+
 	y = 0;
 	std::cout <<"chislo, kolichestvo ";
 	std::cin >> chislo >> kolichestvo; //Число //Количество чисел
@@ -114,7 +160,7 @@ int main()
 	y += -kolichestvo*(1/chislo);
 	}
 	std::cout<<"y = " << y << std::endl;
-
+	file<<"y = " << y << std::endl;
 	std::getchar();
 	std::getchar();
 	return 0;
--- Nedela5-6.cpp
+++ Nedela5-6.cpp
@@ -1,15 +1,21 @@
 #include <iostream>
 #include <math.h> 
+#include <fstream>
 
+using namespace std;
 
 int main()
 {
-	int arr[20]; //Инициализируем массив размером 20 ячеек
+	std::fstream file; // создаем объект класса ifstream
+	file.open("Nedela5-6_Storage.txt",std::ios::out); // открываем файл
+	//file<<""<<std::endl;
+
+	int arr[20]; //Инициализируем массив размером 20 ячеек 
 	int Zadanie2c = 0; //Переменная для задания 2с.
 	int Zadanie2g = 0; //Переменная для задания 2g.
 	int Zadanie5max = 0; int Zadanie5indexmax = 0; //Переменные для задания 5 Максимальный элемент массива
 	int Zadanie5min = 20; int Zadanie5indexmin = 0; 
-	int Zadanie4ot = 0; int Zadanie4pol = 0; //Переменные для задания 4
+	int Zadanie4ot = 1; int Zadanie4pol = 1; //Переменные для задания 4
 	int ZadannoeChislo = 0; int Kolichestvo = 0; std::cin >> ZadannoeChislo; // Ввод заданного числа для задания 10
 	int Zadanie14с = 0; //Задание 14
 
@@ -18,14 +24,14 @@ int main()
        arr[i] = rand() % 20 -10; //Задание 1с. Заполняем каждый элемент массива случайными чисслами в диапазоне [-10;10] 
 	   //Задание 2с. Вычислить сумму элементов массива больших 3.
 	   if (arr[i] > 3) { Zadanie2c+=arr[i]; }; //Задание 2.
-	   //Задание 2п. Вычислить сумму элементов массива меньших 5.
+	   //Задание 2g. Вычислить сумму элементов массива меньших 5.
 	   if (arr[i] > 3) { Zadanie2g+=arr[i]; }; //Задание 2g
 	   //Задание 5. Найти наибольший/наименьший элемент массива и его номер по порядку
 	   if (arr[i] > Zadanie5max) { Zadanie5max = arr[i]; Zadanie5indexmax=i; }; //Наибольший элемент массива и его индекс
 	   if (arr[i] < Zadanie5min) { Zadanie5min = arr[i]; Zadanie5indexmin=i; }; //Наименьший элемент массива и его индекс
 	   //Задание 4. Вычислить произведение отрицательных/положительных элементов массива
-	   if (arr[i] < 0) { Zadanie4ot*=arr[i]; }; 
-	   if (arr[i] > 0) { Zadanie4pol*=arr[i]; }; 
+	   if (arr[i] < 0) { Zadanie4ot=Zadanie4ot*arr[i]; }; 
+	   if (arr[i] > 0) { Zadanie4pol=Zadanie4pol*arr[i]; }; 
 	   //Задание 10. Посчитать количество (и вывести на экран) элементов массива, больших заданного числа
 	   if (arr[i] > ZadannoeChislo) { Kolichestvo++; }; 
 	   //Задание 12. Увеличить все нечётные элементы массива на 3, а чётные в 2 раза
@@ -35,8 +41,12 @@ int main()
 
 	}
 	std::cout << Kolichestvo << std::endl;//Вывод на экран числа для задания 10
-
-
+	file<<"Zadanie2c "<<Zadanie2c<<std::endl;
+	file<<"Zadanie2g "<<Zadanie2g<<std::endl;
+	file<<"Zadanie5 "<<"Max "<<Zadanie5max<<" index "<<Zadanie5indexmax<<" Min "<<Zadanie5min<<" index "<<Zadanie5indexmax<<::endl;
+	file<<"Zadanie4 "<<"ot "<<Zadanie4ot<<" pol "<<Zadanie4pol<<std::endl;
+	file<<"Zadanie10 "<<Kolichestvo<<std::endl;
+	file<<"Zadanie14c "<<Zadanie14с<<std::endl;
 	std::getchar();
 	std::getchar();
 	return 0;
